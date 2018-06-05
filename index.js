e7af2215c6a32ad5275557b52cd75aa141879a71
BTobey <thenameisorca@gmail.com>
2018-06-05 00:56:07 -0700

    adding code
    added code

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-asdf
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyparser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local'),Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/loginapp');
+var db = mongoose.connection;
+
+var route = require('./routes/index');
+var users = require('./routes/users');
+
+// Init loginapp
+var app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
